ff2c11ac447b7e901b87067bd51e44588a041157
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 10:33:22 -0800

    create tests for save, getAll, deleteAll - none will pass til all 3 methods created

--- tests/StudentTest.php
+++ tests/StudentTest.php
@@ -97,6 +97,68 @@
           $this->assertEquals($enrollment_date_update, $result);
         }
 
+        function test_save()
+        {
+            // Arrange
+            $name = "Maggie";
+            $enrollment_date = "2017-02-28";
+            $id = 1;
+            $test_student = new Student($name, $enrollment_date, $id);
+
+            // Act
+            $test_student->save();
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals([$test_student], $result);
+        }
+
+        function test_getAll()
+        {
+            // Arrange
+            $name = "Maggie";
+            $enrollment_date = "2017-02-28";
+            $id = 1;
+            $test_student = new Student($name, $enrollment_date, $id);
+            $test_student->save();
+
+            $name = "Daisy";
+            $enrollment_date = "2017-03-01";
+            $id = 2;
+            $test_student2 = new Student($name, $enrollment_date, $id);
+            $test_student2->save();
+
+            // Act
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals([$test_student, $test_student2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            // Arrange
+            $name = "Maggie";
+            $enrollment_date = "2017-02-28";
+            $id = 1;
+            $test_student = new Student($name, $enrollment_date, $id);
+            $test_student->save();
+
+            $name = "Daisy";
+            $enrollment_date = "2017-03-01";
+            $id = 2;
+            $test_student2 = new Student($name, $enrollment_date, $id);
+            $test_student2->save();
+
+            // Act
+            Student::deleteAll();
+            $result = Student::getAll();
+
+            // Assert
+            $this->assertEquals([], $result);
+        }
+
+
 
     }
 ?>
