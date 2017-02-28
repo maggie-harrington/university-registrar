974bbe351a74062705b47cb658659cd8399b91f4
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 15:08:47 -0800

    create getAll method

--- src/Course.php
+++ src/Course.php
@@ -42,5 +42,20 @@
             $GLOBALS['DB']->exec("INSERT INTO courses (course_name, course_number) VALUES ('{$this->getCourseName()}', '{$this->getCourseNumber()}');");
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
+
+        static function getAll()
+        {
+            $returned_courses = $GLOBALS['DB']->query("SELECT * FROM courses");
+            $all_courses_array = array();
+            foreach($returned_courses as $course)
+            {
+                $course_name = $course['course_name'];
+                $course_number = $course['course_number'];
+                $id = $course['id'];
+                $new_course = new Course($course_name, $course_number, $id);
+                array_push($all_courses_array, $new_course);
+            }
+            return $all_courses_array;
+        }
     }
 ?>
