e14a5c61b0107897ae207b85b370fc4bfbfc969a
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 14:50:09 -0800

    create Course class with construct and getCourseName method, all tests pass

---
+++ src/Course.php
@@ -0,0 +1,20 @@
+<?php
+    class Course
+    {
+        private $course_name;
+        private $course_number;
+        private $id;
+
+        function __construct($course_name, $course_number, $id = null)
+        {
+            $this->course_name = $course_name;
+            $this->course_number = $course_number;
+            $this->id = $id;
+        }
+
+        function getCourseName()
+        {
+            return $this->course_name;
+        }
+    }
+?>
