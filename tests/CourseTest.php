71fa601585b83fe90dcadeaaaaad97ba02504a40
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 15:11:17 -0800

    create test for find method

--- tests/CourseTest.php
+++ tests/CourseTest.php
@@ -159,6 +159,25 @@
             $this->assertEquals([], $result);
         }
 
+        function test_find()
+        {
+            // Arrange
+            $course_name = "General Chemistry";
+            $course_number = "CH 201";
+            $test_course = new Course($course_name, $course_number);
+            $test_course->save();
+
+            $course_name2 = "General Biology";
+            $course_number2 = "BI 201";
+            $test_course2 = new Course($course_name2, $course_number2);
+            $test_course2->save();
+
+            // Act
+            $result = Course::find($test_course->getId());
+
+            // Assert
+            $this->assertEquals($test_course, $result);
+        }
 
 
     }
