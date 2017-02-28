c3b2aab515ca7934923cfe3c67f74a6a600f9cd0
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 15:39:18 -0800

    create routes in app.php for Course class

--- app/app.php
+++ app/app.php
@@ -2,7 +2,7 @@
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Student.php";
-    // require_once __DIR__."/../src/Course.php";
+    require_once __DIR__."/../src/Course.php";
 
     $server = 'mysql:host=localhost:8889;dbname=university_registrar';
     $username = 'root';
@@ -67,6 +67,53 @@
         $student->update($name, $enrollment_date);
 
         return $app['twig']->render('students.html.twig', array('students' => Student::getAll()));
+    });
+
+    // start of courses routes:
+    
+    // routes from index to courses page, displays all courses
+    $app->get("/courses", function() use ($app) {
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    // add a course
+    $app->post("/courses/add", function() use ($app) {
+        $course = new Course($_POST['course_name'], $_POST['course_number']);
+        $course->save();
+
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    // delete all courses
+    $app->delete("/courses/delete_all", function() use ($app) {
+        Course::deleteAll();
+
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    // delete a course
+    $app->delete("/courses/{id}/delete", function($id) use ($app) {
+        $course = Course::find($id);
+        $course->delete();
+
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
+    });
+
+    // edit a course, routes from courses page to course edit page
+    $app->get("/courses/{id}/edit", function($id) use ($app) {
+        $course = Course::find($id);
+
+        return $app['twig']->render('course_edit.html.twig', array('course' => $course));
+    });
+
+    // submit edit to a course, routes back to courses page from course edit page
+    $app->patch("/courses/{id}/submit_edit", function($id) use ($app) {
+        $course_name = $_POST['course_name'];
+        $course_number = $_POST['course_number'];
+        $course = Course::find($id);
+        $course->update($course_name, $course_number);
+
+        return $app['twig']->render('courses.html.twig', array('courses' => Course::getAll()));
     });
 
     return $app;
