62e4453e9f20a2cbe9fbde0b16baeeea0bf59878
Maggie Harrington <maggie.harrington@gmail.com>
2017-02-28 09:54:35 -0800

    create Student class with construct, getters and setters

---
+++ src/Student.php
@@ -0,0 +1,41 @@
+<?php
+    class Student
+    {
+        private $name;
+        private $enrollment_date;
+        private $id;
+
+        function __construct($name, $enrollment_date, $id = null)
+        {
+            $this->name = $name;
+            $this->enrollment_date = $enrollment_date;
+            $this->id = $id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function getEnrollmentDate()
+        {
+            return $this->enrollment_date;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = $new_name;
+        }
+
+        function setEnrollmentDate($new_enrollment_date)
+        {
+            $this->enrollment_date = $new_enrollment_date;
+        }
+
+    }
+?>
